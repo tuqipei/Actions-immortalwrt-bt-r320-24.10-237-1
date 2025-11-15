7d5eecc7fa0c0bc74d96409e905b63fcd49cd141
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-11-15 22:10:24 +0800

    Update filogic.mk

--- dts/filogic.mk
+++ dts/filogic.mk
@@ -1826,7 +1826,7 @@ define Device/bt_r320-emmc
   DEVICE_MODEL := R320 eMMC
   DEVICE_DTS := mt7981b-bt-r320-emmc
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware automount coremark blkid blockdev fdisk f2fsck mkf2fs kmod-mmc
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware automount coremark blkid blockdev fdisk f2fsck mkf2fs kmod-mmc automount
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
   KERNEL_INITRAMFS := kernel-bin | lzma | \
 	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
