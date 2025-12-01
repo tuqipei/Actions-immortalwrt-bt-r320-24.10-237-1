eec7bc2cb60a6bab87d10906121d24050e3067f5
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-12-01 23:10:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,9 +7,11 @@
 
 # 1. 核心：通过修改 feeds.conf.default 来替换 Golang
 # 移除官方 feeds 里的 golang 定义（如果有）
-sed -i '/golang/d' feeds.conf.default
+#sed -i '/golang/d' feeds.conf.default
 
 # 添加 sbwml 的 golang 源
 # 这里的 golang 会在 update feeds 时被拉取到 feeds/golang 目录
-#echo 'src-git golang https://github.com/sbwml/packages_lang_golang;25.x' >> feeds.conf.default
-echo 'src-git golang https://github.com/sbwml/packages_lang_golang' >> feeds.conf.default
+# echo 'src-git golang https://github.com/sbwml/packages_lang_golang;25.x' >> feeds.conf.default
+# echo 'src-git golang https://github.com/sbwml/packages_lang_golang' >> feeds.conf.default
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/orgx2812/golang feeds/packages/lang/golang
